90d2915d5bc336b4fa189907dc8499c90e289cea
松本隆 <takashidoesitright@gmail.com>
2019-05-22 11:23:05 +0900

    Initial Commit

---
+++ SwiftWorkShopTests/SwiftWorkShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftWorkShopTests.swift
+//  SwiftWorkShopTests
+//
+//  Created by 松本隆 on 2019/05/22.
+//  Copyright © 2019 mimpro. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftWorkShop
+
+class SwiftWorkShopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
